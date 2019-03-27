45dc147436c77dd891fa785f5d9268907908c243
BackrndSource <back.rndsource@gmail.com>
2019-03-27 11:20:45 +0000

    Update es.lua

--- locales/es.lua
+++ locales/es.lua
@@ -1,12 +1,19 @@
 Locales['es'] = {
-
 	['valid_this_purchase'] = '¿Validar esta compra?',
-	['yes'] = 'Si',
-	['no'] = 'No',
-	['name_outfit'] = 'Nombre del traje ?',
-	['not_enough_money'] = 'No tienes suficiente dinero',
-	['press_menu'] = 'Presiona ~INPUT_CONTEXT~ para acceder al menú',
-	['clothes'] = 'Ropa',
-	['you_paid'] = 'Has pagado €',
-	['save_in_dressing'] = '¿Quieres darle un nombre a tu vestuario?',
+  	['yes_valid_purchase'] = 'si (<span style="color: green;">%s€</span>)',
+	['yes'] = 'si',
+	['no'] = 'no',
+	['name_outfit'] = '¿Nombre del conjunto?',
+	['not_enough_money'] = 'no tienes suficiente dinero...',
+	['press_menu'] = 'pulsa ~INPUT_CONTEXT~ para comprar ropa',
+	['clothes'] = 'ropa',
+	['you_paid'] = 'has pagado €',
+	['save_in_dressing'] = '¿Quieres guardar tu conjunto?',
+	['shop_clothes'] = 'comprar ropa',
+	['player_clothes'] = 'cambiar conjunto - Guardarropa',
+	['shop_main_menu'] = 'tienda de ropa',
+	['saved_outfit'] = 'su conjunto se guardó en el guardarropa. Gracias por su visita !',
+	['loaded_outfit'] = 'ha recuperado su conjunto del guardarropa. Gracias por su visita !',
+	['suppr_cloth'] = 'eliminar conjunto - Guardarropa',
+	['supprimed_cloth'] = 'el conjunto fue eliminado del guardarropa.'
 }
