4431610f8762daa317cb51577441f388b9e4126a
Arpikudos <patu.tuomineeen@gmail.com>
2019-08-12 08:11:25 -0700

    Create fi.lua

---
+++ locales/fi.lua
@@ -0,0 +1,20 @@
+Locales['fi'] = {
+
+	['valid_this_purchase'] = 'Varmista ostoksesi?',
+	['yes'] = 'Kyllä',
+	['no'] = 'Ei',
+	['name_outfit'] = 'Ulkoasun nimi?',
+	['not_enough_money'] = 'Sinulla ei ole tarpeeksi rahaa',
+	['press_menu'] = 'Paina ~INPUT_CONTEXT~ avataksesi menun',
+	['clothes'] = 'Vaatteet',
+	['you_paid'] = 'Maksoit $',
+	['save_in_dressing'] = 'Haluatko nimetä ulkoasusi?',
+	['shop_clothes'] = 'Vaatekauppa',
+	['player_clothes'] = 'Vaatekaappi',
+	['shop_main_menu'] = 'Tervetuloa, mitä tänään puettaisiin?',
+	['saved_outfit'] = 'Asusteesi on tallennettu vaatekappiin!',
+	['loaded_outfit'] = 'Otit vaatteesi vaatekaapista',
+	['suppr_cloth'] = 'Poista asuste',
+	['supprimed_cloth'] = 'Asuste poistettu vaatekaapista'
+
+}
